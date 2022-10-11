578e447f656c36b9f98f5de2bbf55d108dfc8ab0
Piola Evania <piolaevania@gmail.com>
2022-10-11 10:59:00 +0800

    Delete C directory

--- C/PRAK002-2210817220018-Piola Evania.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main ()
-{
-    printf("Selamat Pagi, Piola Evania\n");
-    printf("Selamat Siang, Piola Evania\n");
-    printf("Selamat Malam, Piola Evania\n");
-    return 0;
-}
