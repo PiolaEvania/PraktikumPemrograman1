a6a1782ff7ae6862d9291acaf202da2873e00c50
Piola Evania <piolaevania@gmail.com>
2022-10-14 23:23:10 +0800

    Add files via upload

---
+++ Modul 1/Soal 2/PRAK002-2210817220018-Piola Evania.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main ()
+{
+    printf("Selamat Pagi, Piola Evania\n");
+    printf("Selamat Siang, Piola Evania\n");
+    printf("Selamat Malam, Piola Evania\n");
+    return 0;
+}
