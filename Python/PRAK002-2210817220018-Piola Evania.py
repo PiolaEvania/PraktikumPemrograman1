85a054d83d60a8a6cbede55ea7a67e6ca569575f
Piola Evania <piolaevania@gmail.com>
2022-10-14 23:12:02 +0800

    Delete Python directory

--- Python/PRAK002-2210817220018-Piola Evania.py
+++
@@ -1,3 +0,0 @@
-print("Selamat Pagi, Piola Evania")
-print("Selamat Siang, Piola Evania")
-print("Selamat Malam, Piola Evania")
--- Python/PRAK109-2210817220018-Piola Evania.py
+++
@@ -1,7 +0,0 @@
-pyz = 958730
-p = 5
-jp = pyz/p
-
-print("Jumlah pasukan yang dibawa Yu Zhong = ", pyz)
-print("Jumlah pahlawan = ", p)
-print("Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah ", round(jp), ("pasukan"))
